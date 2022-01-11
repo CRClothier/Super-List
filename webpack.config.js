399c70b4fa9975cfeab7803616674ba81eb122a3
CRClothier <c.r.clothier@gmail.com>
2022-01-11 15:00:27 +0000

    Add linters

--- webpack.config.js
+++ webpack.config.js
@@ -5,24 +5,24 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devServer: {
-      static: './dist',
-    },
+    static: './dist',
+  },
   plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html'
-      }),
-    ],
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    },
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
